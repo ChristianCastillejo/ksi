b6eb5c49b86fcdc32a1c3f0916f1bf6631c99d22
Christian Castillejo <christiancastillejo@pm.me>
2025-11-18 13:28:09 +0000

    chore: init next.js 16 project and base configuration

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+    cacheComponents: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.ctfassets.net',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default withNextIntl(nextConfig);
