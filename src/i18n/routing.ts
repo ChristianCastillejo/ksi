20f51d1befccdec03991edc227020c5a916d219e
Christian Castillejo <christiancastillejo@pm.me>
2025-11-20 14:08:48 +0000

    feat(i18n): configure next-intl and localized dictionaries

---
+++ src/i18n/routing.ts
@@ -0,0 +1,16 @@
+import { defineRouting } from "next-intl/routing";
+import { createNavigation } from "next-intl/navigation";
+
+export const routing = defineRouting({
+    locales: ["en", "hi"],
+    defaultLocale: "en",
+    localePrefix: "as-needed",
+});
+
+export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
+
+export type Locale = (typeof routing.locales)[number];
+
+export function isValidLocale(locale: string): locale is Locale {
+    return (routing.locales as readonly string[]).includes(locale);
+}
